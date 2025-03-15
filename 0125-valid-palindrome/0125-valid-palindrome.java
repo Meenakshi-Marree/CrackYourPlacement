24b7e310e9f5278f578b39218d8350b4bfd95674
Meenakshi-Marree <151535919+Meenakshi-Marree@users.noreply.github.com>
2025-03-15 13:29:45 +0530

    Time: 16 ms (16.9%), Space: 48.5 MB (5.1%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        return isPalindromeHelper(s,0,s.length()-1);
+    }
+    public boolean isPalindromeHelper(String s,int left,int right){
+        if(left>right) return true;
+        if(s.charAt(left)!=s.charAt(right)) return false;
+        return isPalindromeHelper(s,left+1,right-1);
+    }
+}
