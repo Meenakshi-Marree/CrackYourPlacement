6330f5a6ade93420fc4431a8d5298d23d9484331
Meenakshi <151535919+Meenakshi-Marree@users.noreply.github.com>
2025-08-14 14:15:45 +0530

    Time: 4 ms (100%), Space: 44.5 MB (17.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int revNo=0;
-        int dup=x;
-        while(x>0){
-            int ld = x%10;
-            revNo=(revNo*10)+ld;
-            x=x/10;
-            
-        }
-        if(revNo==dup){
+       int reverseNo = 0;
+       int dup =x;
+       while(x>0){
+        int ld = x%10;
+        x=x/10;
+
+        reverseNo = (reverseNo*10)+ld;
+
+        
+       }
+       if(reverseNo == dup){
             return true;
         }else{
             return false;
