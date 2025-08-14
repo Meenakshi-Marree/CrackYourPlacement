0b7951c960235b29f27d7c8aa96919e2e01a730b
Meenakshi <151535919+Meenakshi-Marree@users.noreply.github.com>
2025-08-14 18:21:30 +0530

    Time: 9 ms (96.28%), Space: 57 MB (47.23%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,27 +11,29 @@
 class Solution {
     //find middle
     public ListNode findMiddle(ListNode head){
-        ListNode slow=head;
-        ListNode fast=head.next;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
+       ListNode slow=head;
+       ListNode fast = head.next;
+       while(fast!=null && fast.next!=null){
+        slow =slow.next;
+        fast=fast.next.next;
+       }
+       return slow;
     }
     public ListNode mergeTwoLists(ListNode list1,ListNode list2){
-        ListNode dummy=new ListNode(-1);
-        ListNode temp=dummy;
+        ListNode dummy = new ListNode();
+        ListNode temp =dummy;
         while(list1!=null && list2!=null){
-            if(list1.val < list2.val){
+            if(list1.val<list2.val){
                 temp.next=list1;
-                list1=list1.next;
-                temp=temp.next;
-            }else{
-                 temp.next=list2;
-                 list2=list2.next;
-                temp=temp.next;
+                list1 = list1.next; 
+                temp =temp.next;
             }
+           
+            else{
+               temp.next=list2;
+                list2 = list2.next;
+                temp = temp.next;
+            }  
         }
         if(list1!=null){
             temp.next=list1;
@@ -42,16 +44,16 @@ class Solution {
     }
     public ListNode sortList(ListNode head) {
 
-        if(head == null || head.next==null) return head;
-        ListNode middle=findMiddle(head);
-        ListNode right=middle.next;
-        middle.next=null;
+        if(head==null || head.next==null) return head;
 
-        ListNode left=head;
+        ListNode middle = findMiddle(head);
+        ListNode right = middle.next;
+        middle.next = null;
 
-        left=sortList(left);
-        right=sortList(right);
+        ListNode left = head;
 
+        left = sortList(left);
+        right = sortList(right);
         return mergeTwoLists(left,right);
         
     }
